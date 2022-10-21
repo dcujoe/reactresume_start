f36614a620812e93bf1a9b8a6d8d883249108d90
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-10-21 11:03:05 +0200

    setup App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css'
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Skills from './components/pages/skills/Skills';
+import Experiences from './components/pages/experiences/Experiences';
+import Education from './components/pages/education/Education';
+import Portfolios from './components/pages/portfolios/Portfolios';
 
 
-export default function App() {
+function App() {
   return(
-    <div className="App">
-      <h1>Hello I am daniel Agbenyega</h1>
-    </div>
+   <>
+   <Router> 
+   <Routes>
+   <Route path="/" element={<Home />}></Route>
+   <Route path="experiences" element={<Experiences />}></Route>
+     
+     <Route path="education" component={Education} />
+     <Route path="skills" component={Skills} />
+     <Route path="portfolios" component={Portfolios} />
+   </Routes>
+   </Router>
+   </>
   )
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,5 @@ const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
