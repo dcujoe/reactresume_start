66e9563387080b21643af85a474b0f28350bce54
John Doe <50689568+dcujoe@users.noreply.github.com>
2022-10-22 08:00:27 +0200

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import Experiences from './pages/experiences/Experiences';
 import Education from './pages/education/Education';
 import Portfolios from './pages/portfolios/Portfolios';
 import Skills from './pages/skills/Skills';
-import { render } from '@testing-library/react';
+
 
 export default function Home() {
    
@@ -16,12 +16,12 @@ export default function Home() {
            <Navbar />
            <div className="container">
                <div className="row">
-                   /* profile is taking one side of the row representing m3 from Bootstrap*/
+
                    <div className="col s12 m3">
                        <Profile />
                    </div>
 
-                   /* The other section contains other parts of the row m9. Right side of the screen */
+                  
                    <div className="col s12 m9">
                        <About />
                        <Skills />
